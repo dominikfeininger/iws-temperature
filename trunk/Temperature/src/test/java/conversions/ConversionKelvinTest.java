3a9f7248e9d329ac22aa6bad75a623a5158ad6d0
benjamin.lautenschlaeger@gmail.com <benjamin.lautenschlaeger@gmail.com@61de0200-a78a-4c5d-7fc4-caa1d8ed6dd9>
2013-02-02 15:55:20 +0000

    new test case for fahrenheit

---
+++ trunk/Temperature/src/test/java/conversions/ConversionKelvinTest.java
@@ -0,0 +1,24 @@
+package test.java.conversions;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConversionKelvinTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assert(true);
+	}
+
+}
