56b735f0778ac2c8543c6d44764a7f5ca3e8a5da
benjamin.lautenschlaeger@gmail.com <benjamin.lautenschlaeger@gmail.com@61de0200-a78a-4c5d-7fc4-caa1d8ed6dd9>
2013-02-02 10:57:55 +0000

    Initial Upload

---
+++ Temperature/src/conversions/Conversion.java
@@ -0,0 +1,13 @@
+package conversions;
+
+public interface Conversion {
+
+	public abstract double fahrenheitToCelsius(double fahrenheit);
+
+	public abstract double celsiusToFahrenheit(double celsius);
+
+	public abstract double celsiusToKelvin(double celsius);
+
+	public abstract double kelvinToCelsius(double kelvin);
+
+}
