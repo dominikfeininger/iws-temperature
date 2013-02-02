6deb1c9ae472a752b9c9ef3cb7f990c64183822d
benjamin.lautenschlaeger@gmail.com <benjamin.lautenschlaeger@gmail.com@61de0200-a78a-4c5d-7fc4-caa1d8ed6dd9>
2013-02-02 10:57:55 +0000

    Initial Upload

---
+++ trunk/Temperature/src/conversions/ConversionImpl.java
@@ -0,0 +1,39 @@
+package conversions;
+
+/**
+ * 
+ * @author Benjamin
+ *
+ * Formeln:	 
+ * C=K-273.15
+ * K=C+273.15
+ * F=Cx1.8+32
+ * C=(F-32)÷ 1.8
+ *
+ */
+public class ConversionImpl implements Conversion {
+
+	
+	@Override
+	public double fahrenheitToCelsius(double fahrenheit){
+		return (fahrenheit - 32.0) * (5.0/9.0);
+	}
+	
+	
+	@Override
+	public double celsiusToFahrenheit(double celsius){
+		return (celsius * (9.0/5.0)) + 32.0;
+	}
+	
+	
+	@Override
+	public double celsiusToKelvin(double celsius){
+		return (celsius + 273.15);
+	}
+	
+	
+	@Override
+	public double kelvinToCelsius(double kelvin){
+		return (kelvin - 273.15);
+	}	
+}
