11dd4318904812b78d87ab151f855c3a89801415
dominik.feininger <dominik.feininger@61de0200-a78a-4c5d-7fc4-caa1d8ed6dd9>
2013-02-03 22:39:36 +0000

    updated Tests with Parameters

--- Temperature/src/test/java/conversions/ConversionTest.java
+++ Temperature/src/test/java/conversions/ConversionTest.java
@@ -1,15 +1,13 @@
 package test.java.conversions;
-import static org.junit.Assert.*;
 
+import static org.junit.Assert.*;
 import main.java.conversions.Conversion;
 import main.java.conversions.ConversionImpl;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 
-public class ConversionCelsiusTest {
+public class ConversionTest {
 
 	Conversion converter;
 	
@@ -18,20 +16,19 @@ public class ConversionCelsiusTest {
 		converter = new ConversionImpl();
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	//1 a)
+	//gruen
 	@Test
-	public void testCelsiusToFahrenheit() {
+	public void testCelsiusToFahrenheit() throws InterruptedException{
 		double celsius = 20;
 		double expectedFahrenheit = 68;
 		
 		double fahrenheit = converter.celsiusToFahrenheit(celsius);
-		
 		assertEquals(expectedFahrenheit, fahrenheit, 0.0);
 	}
 	
+	//1 b)
+	//rot -> wird gefixt
 	@Test
 	public void testFahrenheitToCelsius() {
 		double expectedCelsius = 20;
@@ -40,5 +37,21 @@ public class ConversionCelsiusTest {
 		double celsius = converter.fahrenheitToCelsius(fahrenheit);		
 		assertEquals(expectedCelsius, celsius, 0.0);
 	}
-
+	
+	//2
+	@Test (timeout = 10)
+	public void testCelsiusToFahrenheitPerformance() throws InterruptedException {
+		double celsius = 20;
+		double expectedFahrenheit = 68;
+		
+		double fahrenheit = converter.celsiusToFahrenheit(celsius);
+		assertEquals(expectedFahrenheit, fahrenheit, 0.0);
+	}
+	
+	//3
+	@Test (expected = IllegalArgumentException.class) 
+	public void testKelvinToCelsius() throws IllegalArgumentException {
+		converter.kelvinToCelsius(-1);
+	}
+	
 }
